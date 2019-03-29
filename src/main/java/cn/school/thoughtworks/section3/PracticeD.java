421e46d02121127bc867fb46983cf11f209aedaa
Qing Xiang <1355496697@qq.com>
2019-03-29 22:36:20 +0800

    Section3 PracticeD commit

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,34 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        Iterator iterator = collectionA.iterator();
+        while (iterator.hasNext()) {
+            String c = (String) iterator.next();
+            String key = c.substring(0, 1);
+            if (map.get(key) == null)
+                map.put(key, 0);
+            int num = 0;
+            if (c.length() > 1) {
+                String numStr = c.substring(2, 3);
+                num = Integer.valueOf(numStr);
+            } else num = 1;
+            map.put(key, map.get(key) + num);
+        }
+        Iterator iterator1 = object.get("value").iterator();
+        while (iterator1.hasNext()) {
+            String str = (String) iterator1.next();
+            Integer value = map.get(str);
+            if (value != null)
+                map.put(str, value-value/3);
+        }
+        return map;
     }
 }
