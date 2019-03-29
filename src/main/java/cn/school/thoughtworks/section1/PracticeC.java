4b6c5be24b6e5ee5eaa6b8a920c9761aa5b9db16
Qing Xiang <1355496697@qq.com>
2019-03-29 21:56:18 +0800

    PracticeC commit

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,17 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        Collection exists=new ArrayList<String>(collection1);
+        Collection notexists=new ArrayList<String>(collection1);
+        exists.removeAll(collection2.get("value"));
+        notexists.removeAll(exists);
+        return (List<String>) notexists;
     }
 }
