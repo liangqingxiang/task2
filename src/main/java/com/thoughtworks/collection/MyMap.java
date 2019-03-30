c16ca6c6655d73225f5748f22c31b44e761b5b23
Qing Xiang <1355496697@qq.com>
2019-03-30 22:19:46 +0800

    MyMap.java commit

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,22 +18,77 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        List<Integer> tripleList = new ArrayList<>();
+
+        for(int i = 0; i<array.size(); i++){
+            tripleList.add(array.get(i)*3);
+        }
+        return tripleList;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> result = new ArrayList<>();
+        for(int i=0; i<array.size(); i++){
+            int index = array.get(i) - 1;
+            result.add(letterList.get(index));
+        }
+
+        return result;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        List<String> result = new ArrayList<>();
+        for(int i=0; i<array.size(); i++){
+
+            int currentElement = array.get(i);
+
+            if(currentElement <= 26){
+                result.add(letterList.get(currentElement-1));
+            }else{
+
+                int firstIndex = currentElement/26;
+                int secondIndex = currentElement%26;
+
+                if(secondIndex == 0){
+                    secondIndex = 26;
+                    firstIndex--;
+                }
+
+                String mapLetter = letterList.get(firstIndex-1) + letterList.get(secondIndex-1);
+                result.add(mapLetter);
+            }
+        }
+
+        return result;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        for(int i =0; i<array.size(); i++){
+            for(int j=i+1; j<array.size(); j++){
+
+                if (array.get(i) < array.get(j)){
+                    Integer temp = array.get(i);
+                    array.set(i, array.get(j));
+                    array.set(j, temp);
+                }
+            }
+        }
+
+        return array;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        for(int i =0; i<array.size(); i++){
+            for(int j=i+1; j<array.size(); j++){
+
+                if (array.get(i) > array.get(j)){
+                    Integer temp = array.get(i);
+                    array.set(i, array.get(j));
+                    array.set(j, temp);
+                }
+            }
+        }
+
+        return array;
     }
 }
