f91a4ef65526f27db16bc260f09379ea44206950
Qing Xiang <1355496697@qq.com>
2019-04-13 22:31:11 +0800

    practice11 commit

--- src/main/java/practice11/AssignLeaderListener.java
+++ src/main/java/practice11/AssignLeaderListener.java
@@ -1,4 +1,5 @@
 package practice11;
 
 public interface AssignLeaderListener {
+    void notifyAssignLeader(Student student, Klass klass);
 }
--- src/main/java/practice11/JoinListener.java
+++ src/main/java/practice11/JoinListener.java
@@ -1,4 +1,5 @@
 package practice11;
 
 public interface JoinListener {
+    void notifyJoin(Student student, Klass klass);
 }
